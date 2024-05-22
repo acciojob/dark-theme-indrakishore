b6db7a4a4a5b3e8735be4ea40febe10352b6a04a
Indra Kishore Kumar <55511698+indrakishore@users.noreply.github.com>
2024-05-23 02:40:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
 
+function swapTheme() {
+	var app = document.getElementById("app");
+	var swap = document.getElementById("swap");
+
+	if(app.class == "day") {
+		app.class = "night";
+	} else {
+		app.class = "day";
+	}
+}
