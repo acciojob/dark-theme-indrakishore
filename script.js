fcfe91cf4a1d58e271e26915227b65911a9e0e85
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2024-05-23 03:06:38 +0530

    Dark theme switcher implemented

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ function swapTheme() {
 	var app = document.getElementById("app");
 	var swap = document.getElementById("swap");
 
-	if(app.class == "day") {
-		app.class = "night";
+	if(app.className == "day") {
+		app.className = "night";
 	} else {
-		app.class = "day";
+		app.className = "day";
+	}
+
+	if(swap.className == "button_day") {
+		swap.className = "button_night";
+	} else {
+		swap.className = "button_day";
 	}
 }
